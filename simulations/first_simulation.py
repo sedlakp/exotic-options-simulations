06b13328baa4d2b6c2d1b6f02b8857c68ec89835
Petr Sedlak <petrsedlak@me.com>
2020-03-20 00:08:08 +0100

    Add comments and change example call

--- simulations/first_simulation.py
+++ simulations/first_simulation.py
@@ -17,9 +17,14 @@ def mean_reverting(s,x,*,positionFlag="c",
         simulation of european put and call vanilla options
 
         plot with simulation paths is created as well
+
+        v - volatility
+        r - discount rate
     """
 
     dt = time_to_maturity/steps
+
+    # Check whether call or put is requested
     position = 0
     if positionFlag == "c":
         position = 1
@@ -29,36 +34,40 @@ def mean_reverting(s,x,*,positionFlag="c",
         raise ValueError(f"Position can be either p or c, not {positionFlag}")
 
     sum=0.0
-
     paths = []
-
+    #
     for i in range(1,simulations):
         path = []
         st = s
+        # simulate path
         for _ in range(0,steps):
             # https://math.stackexchange.com/questions/345773/how-the-ornstein-uhlenbeck-process-can-be-considered-as-the-continuous-time-anal
             ds = kappa * (theta-st) * dt + v * math.sqrt(dt) * np.random.normal(0,1)
             st = st + ds
             path.append(st)
+        # accumulate payoff from each simulated path
         sum = sum + max(position * (st - x),0)
         paths.append(path)
 
+    # Return present value
     return (math.exp(-r*time_to_maturity)/simulations)*sum, paths, range(0,steps)
 
+
+# test
 k, paths, time_steps = mean_reverting(
     42,
     40,
     positionFlag="c",
-    time_to_maturity=0.5,
-    steps=50,
+    time_to_maturity=1,
+    steps=250,
     simulations=100,
     kappa=0.0001,
     theta=45,
-    v=0.2,
+    v=2,
     r=0.1)
 
 
-print(k)
+print("Simulated option price: ",k)
 
 for path in paths:
     plt.plot(time_steps, path)
